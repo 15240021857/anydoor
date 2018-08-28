d2a8ffa9720626246fd6c8e630713c32d47796e5
wuzhenhua <657615322@qq.com>
2018-08-28 16:30:58 +0800

    stat file or dirctory

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
 const http = require('http')
+const fs = require('fs')
+const path = require('path')
 const conf = require('./config/defaultConfig.js')
 
 const server = http.createServer((req,res) => {
-    res.statusCode = 200
-    res.setHeader('content-type','text/plain')
-    res.end('hello nodejs')
+    const filePath = path.join(conf.root, req.url)
+    console.info(conf.root)
+    console.info(req.url)
+    fs.stat(filePath, (err,stats) => {
+        if(err){
+            res.statusCode = 404
+            res.setHeader('content-type','text/plain')
+            res.end('${filePath} is not a file or directory')
+            return
+        }
+
+        if(stats.isFile()) {
+            res.statusCode = 200
+            res.setHeader('content-type','text/plain')
+            // fs.readFile(filePath,(err,data) => {
+            //     res.end(data)
+            // })
+            fs.createReadStream(filePath).pipe(res)
+        }else if(stats.isDirectory){
+            fs.readdir(filePath,(err,files) => {
+                res.statusCode = 200
+                res.setHeader('content-type','text/plain')
+                res.end(files.join(','))
+            })
+        }
+    })
 })
 
 server.listen(conf.port,conf.hostname, () => {
